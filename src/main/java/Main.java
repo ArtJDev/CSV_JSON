272d25fb442e3cd7f1614a8912d8bc16331a5f5b
ArtJDev <artemkurilenko1983@yandex.ru>
2022-03-31 21:03:19 +0300

    1st part complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
@@ -6,6 +9,8 @@ import com.opencsv.exceptions.CsvValidationException;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,25 +19,36 @@ public class Main {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
         List<Employee> list = parseCSV(columnMapping, fileName);
+        String json = listToJson(list);
+        System.out.println(json);
     }
 
     private static List<Employee> parseCSV(String[] columnMapping, String fileName) throws IOException {
-        ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
-        strategy.setType(Employee.class);
-        strategy.setColumnMapping("id", "firstName", "lastName", "country", "age");
 
+        List<Employee> staff = new ArrayList<>();
         try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
-            String[] nextLine;
-            while ((nextLine = reader.readNext()) != null) {
-                System.out.println(Arrays.toString(nextLine));
-            }
+
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping("id", "firstName", "lastName", "country", "age");
+
             CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(reader)
                     .withMappingStrategy(strategy)
                     .build();
-
-        } catch (IOException | CsvValidationException e) {
+            staff = csv.parse();
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return staff;
+    }
+
+    private static String listToJson (List<Employee> list){
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+
+        Type listType = new TypeToken<List<Employee>>() {}.getType();
+
+        String json = gson.toJson(list, listType);
+        return json;
     }
 }
