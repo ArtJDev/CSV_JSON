934f5a0957408c65883fb8e3e51b69f80c29bfbb
ArtJDev <artemkurilenko1983@yandex.ru>
2022-04-03 22:11:22 +0300

    Remake parseXML()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,32 +5,67 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import com.opencsv.exceptions.CsvValidationException;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
-        String fileName = "data.csv";
-        List<Employee> list = parseCSV(columnMapping, fileName);
-        String json = listToJson(list);
-        System.out.println(json);
+        String csvFileName = "data.csv";
+        List<Employee> csvList = parseCSV(columnMapping, csvFileName);
+        String csvJson = listToJson(csvList);
+        writeString(csvJson, "data.json");
+//        List<Employee> xmlList = parseXML("data.xml");
+//        String xmlJson = listToJson(xmlList);
+//        writeString(xmlJson, "data2.json");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse("data.xml");
+        Node root = doc.getDocumentElement();
+        System.out.println("Корневой элемент: " + root.getNodeName());
+        read(root);
+
+
+    }
+    private static void read(Node node) {
+        NodeList nodeList = node.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node_ = nodeList.item(i);
+            if (Node.ELEMENT_NODE == node_.getNodeType()) {
+                System.out.println("Текущий узел: " + node_.getNodeName());
+                Element element = (Element) node_;
+                NamedNodeMap map = element.getAttributes();
+                for (int a = 0; a < map.getLength(); a++) {
+                    String attrName = map.item(a).getNodeName();
+                    String attrValue = map.item(a).getNodeValue();
+                    System.out.println("Атрибут: " + attrName + "; значение: " + attrValue);
+                }
+                read(node_);
+            }
+        }
     }
 
-    private static List<Employee> parseCSV(String[] columnMapping, String fileName) throws IOException {
+    private static List<Employee> parseCSV(String[] columnMapping, String csvFileName) {
 
         List<Employee> staff = new ArrayList<>();
-        try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+        try (CSVReader reader = new CSVReader(new FileReader(csvFileName))) {
 
             ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
             strategy.setType(Employee.class);
-            strategy.setColumnMapping("id", "firstName", "lastName", "country", "age");
+            strategy.setColumnMapping(columnMapping);
 
             CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(reader)
                     .withMappingStrategy(strategy)
@@ -42,13 +77,45 @@ public class Main {
         return staff;
     }
 
-    private static String listToJson (List<Employee> list){
+    private static List<Employee> parseXML(String xmlFileName) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(xmlFileName);
+        List<Employee> staff = new ArrayList<>();
+        Node root = doc.getDocumentElement();
+        NodeList nodeList = root.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node_ = nodeList.item(i);
+            if (Node.ELEMENT_NODE == node_.getNodeType()) {
+                Element element = (Element) node_;
+                NamedNodeMap map = element.getAttributes();
+                for (int a = 0; a < map.getLength(); a++) {
+                    staff.add((Employee) map.item(a).getAttributes());
+                }
+            }
+        }
+        return staff;
+    }
+
+    private static String listToJson(List<Employee> list) {
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
 
-        Type listType = new TypeToken<List<Employee>>() {}.getType();
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
 
         String json = gson.toJson(list, listType);
         return json;
+    }
+
+    private static void writeString(String jsonObj, String jsonFile) {
+        JSONParser parser = new JSONParser();
+        try (FileWriter file = new FileWriter(jsonFile)) {
+            JSONArray obj = (JSONArray) parser.parse(jsonObj);
+            file.write(obj.toJSONString());
+            file.flush();
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }
